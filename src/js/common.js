6a7de6487853ddd383aa1b49667f821bc3704160
liu97 <1139472029@qq.com>
2018-04-02 12:00:37 +0800

    编写关于我代码

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,38 @@
-function a_location(a) {
-	a.click(function(){
-	    $('html, body').animate({
-	        scrollTop: $( $.attr(this, 'href') ).offset().top+document.body.scrollTop
+function a_location(a, target, father) {
+	if(a == undefined){ //一定要输入锚点a
+		new Error("必须传入a");
+	}
+	else if(target != undefined){ //如果被点击目标不为空
+		if(father != undefined){  //如果要绑定多个元素，且该元素都在同一个father内
+			father.on('click', target, function(event) {
+				let clickTarget = $(event.target).attr('href') ? $(event.target) : $(event.target).find(a).eq(0);
+				$('html, body').animate({
+				    scrollTop: $(clickTarget.attr('href') ).offset().top+document.body.scrollTop
 
-	    }, 500);
-	    return false;
-	});
+				}, 500);
+				return false;
+				/* Act on the event */
+			});
+		}
+		else{ //只传入单个点击目标和锚点a
+			target.click(function(event) { 
+				$('html, body').animate({
+				    scrollTop: $( a.attr('href') ).offset().top+document.body.scrollTop
+
+				}, 500);
+				return false;
+				/* Act on the event */
+			});
+		}
+	}
+	else if(father == undefined){ //只传入单个锚点
+		a.click(function(){
+		    $('html, body').animate({
+		        scrollTop: $( a.attr('href') ).offset().top+document.body.scrollTop
+
+		    }, 500);
+		    return false;
+		});
+	}
+	
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 $(function () {
 	a_location($("#page_header_icon")); //锚点定位
+	a_location($(".page_nav_a"), $(".page_nav_li"), $("#page_nav_ul")); //锚点定位
+	$("#page_nav_bars").click(function(event) {
+		$("#page_nav_ul").slideToggle("slow");
+		$("#page_nav_bars i:only-child").toggleClass("fa-close fa-bars");
+		return false;
+	});
 })
+$(window).resize(function() {  
+	var width = $(this).width();    
+  	if(width>768){
+  		$("#page_nav_ul").css({
+  			display: 'flex'
+   		});
+  	}
+}); 
