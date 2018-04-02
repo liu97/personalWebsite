7897d1ac9f5c1ed587208d5bff3e83a70ae6c897
liu97 <1139472029@qq.com>
2018-04-02 21:01:59 +0800

    完善导航栏

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,48 @@
 $(function () {
 	a_location($("#page_header_icon")); //锚点定位
 	a_location($(".page_nav_a"), $(".page_nav_li"), $("#page_nav_ul")); //锚点定位
-	$("#page_nav_bars").click(function(event) {
+	$("#page_nav_bars").click(function(event) { //当导航bar出来后的点击事件
 		$("#page_nav_ul").slideToggle("slow");
-		$("#page_nav_bars i:only-child").toggleClass("fa-close fa-bars");
 		return false;
 	});
 })
+$(window).scroll(function(event) {
+	//导航条定位的改变
+	if($(document).scrollTop() >= $(window).height()){
+		$("#page_nav").css({
+			position: 'fixed',
+			top: "0"
+		});
+	}
+	else{
+		$("#page_nav").css({
+			position: 'absolute',
+			top:"100%"
+		});
+	}
+	//导航条活动页面的选定
+	for (let i = 0; i < $(".page_nav_li").length; i++) {
+		if($(document).scrollTop() >= $(window).height() *i){
+			$(".page_nav_li").each(function(index, el) {
+				$(el).removeClass('page_nav_li_active');
+			});
+			$(".page_nav_li").eq(i).addClass('page_nav_li_active');
+
+		}
+	}
+	
+	/* Act on the event */
+});
 $(window).resize(function() {  
-	var width = $(this).width();    
+	var width = $(this).width();        
   	if(width>768){
   		$("#page_nav_ul").css({
   			display: 'flex'
+   		});
+  	}
+  	else{
+  		$("#page_nav_ul").css({
+  			display: 'none'
    		});
   	}
 }); 
