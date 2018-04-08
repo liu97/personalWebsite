dc99d7303e188769627a82affbf5e319cc041496
liu97 <1139472029@qq.com>
2018-04-08 22:09:54 +0800

    编写屏幕改变是category页面的变化代码

--- src/js/category.js
+++ src/js/category.js
@@ -1,4 +1,9 @@
 $(function() {
 	// 为tag标签设置随机颜色
 	random_color($('.category_header_aside_li_a')); 
+
+	$("#category_bars").click(function(event) { //当导航bar出来后的点击事件
+		$("#category_header").slideToggle("slow");
+		return false;
+	});
 })
