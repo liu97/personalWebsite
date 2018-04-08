43b38a8c3dd5561e25eae45cb004de16bdde8ea2
liu97 <1139472029@qq.com>
2018-04-08 13:45:14 +0800

    编写所有文章页面

---
+++ src/js/category.js
@@ -0,0 +1,4 @@
+$(function() {
+	// 为tag标签设置随机颜色
+	random_color($('.category_header_aside_li_a')); 
+})
--- src/js/common.js
+++ src/js/common.js
@@ -36,3 +36,28 @@ function a_location(a, target, father) { //滑动锚点
 	}
 	
 }
+function random_color(list){  //为集合元素添加随机颜色
+	list.each(function(index, el) {
+		let R = Math.floor(Math.random() * 255);
+        let G = Math.floor(Math.random() * 255);
+        let B = Math.floor(Math.random() * 255);
+        $(el).css({
+        		'background-color': 'white',
+        		'color': 'rgb(' + R + ',' + G + ',' + B + ')',
+        		'border':'1px solid rgb(' + R + ',' + G + ',' + B + ')'
+        	});
+        $(el).hover(function() {
+        	$(el).css({
+        		'background-color': 'rgb(' + R + ',' + G + ',' + B + ')',
+        		'color': 'white',
+        		'border':'1px solid #ccc'
+        	});
+        }, function() {
+        	$(el).css({
+        		'background-color': 'white',
+        		'color': 'rgb(' + R + ',' + G + ',' + B + ')',
+        		'border':'1px solid rgb(' + R + ',' + G + ',' + B + ')'
+        	});
+        });
+	});
+}
