93001ede145aa9b4fe42917c0f43857bf34c5466
liu97 <1139472029@qq.com>
2018-04-10 09:04:07 +0800

    优化category页面

--- src/js/category.js
+++ src/js/category.js
@@ -2,7 +2,8 @@ $(function() {
 	// 为tag标签设置随机颜色
 	random_color($('.category_header_aside_li_a')); 
 
-	$("#category_bars").click(function(event) { //当导航bar出来后的点击事件
+	//当导航bar出来后的点击事件
+	$("#category_bars").click(function(event) { 
 		$("#category_header").slideToggle("slow");
 		return false;
 	});
